4b44357fbe270cac507a2e25ccb164e329eeb5d6
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-14 17:52:51 -0300

    feat: generic model to all mongo odm

---
+++ src/models/Mongo.model.ts
@@ -0,0 +1,17 @@
+import { Model } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+class MongoModel<T> implements IModel<T> {
+  constructor(private model:Model<T>) {}
+
+  public async create(payload:T):Promise<T> {
+    const addPayload = await this.model.create(payload);
+
+    return {
+      _id: addPayload._id,
+      ...payload,
+    };
+  }
+}
+
+export default MongoModel;
