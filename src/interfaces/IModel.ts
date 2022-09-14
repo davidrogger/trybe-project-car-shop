3769e73be3e1861a196cc1e440b15d3b724501a8
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-14 15:24:19 -0300

    feat: created new Interface IModel

---
+++ src/interfaces/IModel.ts
@@ -0,0 +1,13 @@
+interface IModel<T> {
+  create(payload:T):Promise<T>;
+  read():Promise<T[]>;
+  readOne(id:string):Promise<T | null>;
+  update(id:string, payload:T):Promise<T | null>;
+  delete(id:string):Promise<T | null>;
+}
+
+export {
+  // A interface deve ser exportada com o nome de IModel e não deve ser exportada de forma padrão.
+  // eslint-disable-next-line
+  IModel,
+};
