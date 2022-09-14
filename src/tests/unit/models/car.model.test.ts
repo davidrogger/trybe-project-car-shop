e94f49aea7dae45add0ef65b5324e27a658c8b4f
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-14 19:56:06 -0300

    test: change car model describes

--- src/tests/unit/models/car.model.test.ts
+++ src/tests/unit/models/car.model.test.ts
@@ -14,12 +14,12 @@ import {
 // External methods mocked
 import { Model } from 'mongoose';
 
-describe('Rota "/cars"', () => {
+describe('Testing CarModel', () => {
   afterEach(() => sinon.restore());
 
   const carModel = new CarModel();
 
-  describe('When using POST to add a car with success', () => {
+  describe('When using model to add a car with success', () => {
     it('Should return the registered payload with an id', async () => {
       sinon.stub(Model, 'create').resolves(carWithId);
 
@@ -29,7 +29,7 @@ describe('Rota "/cars"', () => {
 
   });
 
-  describe('When using GET to get all cars with success', () => {
+  describe('When using model to get all cars with success', () => {
     it('Should return a list with all cars', async () => {
       sinon.stub(Model, 'find').resolves([carWithId]);
 
