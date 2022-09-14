251af16dc56426b771092d5da808c3139c931314
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-14 18:27:53 -0300

    feat: add validation into the vehicle
    add positive number to the buyvalue condition

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -5,7 +5,7 @@ const vehicleZodSchema = z.object({
   year: z.number().gte(1900).lte(2022),
   color: z.string().min(3),
   status: z.union([z.boolean(), z.undefined()]),
-  buyValue: z.number().int(),
+  buyValue: z.number().int().positive(),
 });
 
 type IVehicle = z.infer<typeof vehicleZodSchema>;
