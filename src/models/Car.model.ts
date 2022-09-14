9b7daef00c112e09d47a440dfc3e075093ba92f7
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-14 20:55:24 -0300

    feat: carMongooseSchema
    add all fields int the schema of mongoose  without then it saves only the ID and versionKey in the document

--- src/models/Car.model.ts
+++ src/models/Car.model.ts
@@ -2,7 +2,15 @@ import { model as mongooseModel, Schema } from 'mongoose';
 import MongoModel from './Mongo.model';
 import { ICar } from '../interfaces/ICar';
 
-const carMongooseSchema = new Schema<ICar>();
+const carMongooseSchema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
 
 class CarModel extends MongoModel<ICar> {
   constructor() {
