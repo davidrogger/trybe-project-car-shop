3867a9b15d42ff4961bb9f27f550b130dfdec894
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-14 19:08:06 -0300

    test: model car

---
+++ src/tests/mocks/carMock.ts
@@ -0,0 +1,15 @@
+// Reaproveitando parcialmente mock Criado pela trybe.
+
+export const validCar = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
+export const carWithId = {
+  _id: '4edd40c86762e0fb12000003',
+  ...validCar
+}
--- src/tests/unit/models/car.model.test.ts
+++ src/tests/unit/models/car.model.test.ts
@@ -2,18 +2,27 @@ import * as sinon from 'sinon';
 import chai from 'chai';
 const { expect } = chai;
 
+// test itens
+import CarModel from '../../../models/Car.model';
+
 // cars Mocked
 import {
   validCar,
   carWithId,
 } from '../../mocks/carMock';
 
+// External methods mocked
+import { Model } from 'mongoose';
 
 describe('Rota "/cars"', () => {
+  afterEach(() => sinon.restore());
+
   const carModel = new CarModel();
 
   describe('When add a car with success', () => {
-    it('Should return data record with an id', async () => {
+    it('Should return the registered payload with an id', async () => {
+      sinon.stub(Model, 'create').resolves(carWithId);
+
       const addCarModel = await carModel.create(validCar);
       expect(addCarModel).to.be.deep.equal(carWithId);
     });
