92a77be20a442d91caf03af2993990739eeb663a
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-15 17:12:49 -0300

    feat: add new route to update a car by its id

--- src/routes/car.route.ts
+++ src/routes/car.route.ts
@@ -9,6 +9,7 @@ const carService = new CarService(carMongooseModel);
 const carController = new CarController(carService);
 
 route.get('/:id', (req, res) => carController.readOne(req, res));
+route.put('/:id', (req, res) => carController.update(req, res));
 route.post('/', (req, res) => carController.create(req, res));
 route.get('/', (req, res) => carController.read(req, res));
 
